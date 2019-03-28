ac9e5ad2df482c48082b24a4e6c63d273367fec4
meng.zhang1 <meng.zhang1@capitalonline.net>
2019-03-28 16:35:34 +0800

    Organize the code structure add markdown support

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,6 +4,39 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
 
+const vueMarkdown = {
+  preprocess: (MarkdownIt, source) => {
+    MarkdownIt.renderer.rules.table_open = function () {
+      return '<table class="table">'
+    }
+    MarkdownIt.renderer.rules.fence = utils.wrapCustomClass(MarkdownIt.renderer.rules.fence)
+    return source
+  },
+  use: [
+    [MarkdownItContainer, 'demo', {
+      // 用于校验包含demo的代码块
+      validate: params => params.trim().match(/^demo\s*(.*)$/),
+      render: function(tokens, idx) {
+        
+        var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
+
+        if (tokens[idx].nesting === 1) {
+          var desc = tokens[idx + 2].content;
+          // 编译成html
+          const html = utils.convertHtml(striptags(tokens[idx + 1].content, 'script'))
+          // 移除描述，防止被添加到代码块
+          tokens[idx + 2].children = [];
+
+          return `<demo-block>
+                        <div slot="desc">${html}</div>
+                        <div slot="highlight">`;
+        }
+        return '</div></demo-block>\n';
+      }
+    }]
+  ]
+}
+
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
@@ -74,6 +107,11 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.md$/,
+        loader: 'vue-markdown-loader',
+        options: vueMarkdown
       }
     ]
   },
--- examples/router/index.js
+++ examples/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: r => require.ensure([], () => r(require('../docs/test.md')))
     }
   ]
 })
