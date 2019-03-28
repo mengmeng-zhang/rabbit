5bf39beba1855d6a04af9dc3dcabbe52276d1d84
meng.zhang1 <meng.zhang1@capitalonline.net>
2019-03-28 17:17:16 +0800

    add function

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,8 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const MarkdownItContainer = require('markdown-it-container')
+const striptags = require('./strip-tags')
 
 const vueMarkdown = {
   preprocess: (MarkdownIt, source) => {
@@ -10,19 +12,35 @@ const vueMarkdown = {
       return '<table class="table">'
     }
     MarkdownIt.renderer.rules.fence = utils.wrapCustomClass(MarkdownIt.renderer.rules.fence)
+
+    // ```html `` 给这种样式加个class hljs
+    //  但是markdown-it 有个bug fence整合attr的时候直接加载class数组上而不是class的值上
+    //  markdown-it\lib\renderer.js 71行 这么修改可以修复bug
+    //  tmpAttrs[i] += ' ' + options.langPrefix + langName; --> tmpAttrs[i][1] += ' ' + options.langPrefix + langName;
+    // const fence = MarkdownIt.renderer.rules.fence 
+    // MarkdownIt.renderer.rules.fence = function(...args){
+    //   args[0][args[1]].attrJoin('class', 'hljs')
+    //   var a = fence(...args)
+    //   return a
+    // }
+
+    // ```code`` 给这种样式加个class code_inline
+    const code_inline = MarkdownIt.renderer.rules.code_inline
+    MarkdownIt.renderer.rules.code_inline = function(...args){
+      args[0][args[1]].attrJoin('class', 'code_inline')
+      return code_inline(...args)
+    }
     return source
   },
   use: [
     [MarkdownItContainer, 'demo', {
-      // 用于校验包含demo的代码块
       validate: params => params.trim().match(/^demo\s*(.*)$/),
       render: function(tokens, idx) {
-        
+
         var m = tokens[idx].info.trim().match(/^demo\s*(.*)$/);
 
         if (tokens[idx].nesting === 1) {
           var desc = tokens[idx + 2].content;
-          // 编译成html
           const html = utils.convertHtml(striptags(tokens[idx + 1].content, 'script'))
           // 移除描述，防止被添加到代码块
           tokens[idx + 2].children = [];
--- examples/docs/test.md
+++ examples/docs/test.md
@@ -1,2 +1,3 @@
-# test
-> Hello World
+## test
+
+- test.md
